82f1b9f0caf6bd095a480dc023bb98cbba939225
zsonagy <gerzsonbigdata@gmail.com>
2019-02-09 00:41:37 +0100

    bug fix

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-java -jar target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:51000
+#java -jar target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:51000
+yarn target/jar example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar
