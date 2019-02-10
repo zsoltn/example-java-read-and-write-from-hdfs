ba8897b1f4a04196f6c1c0750b49e333917aeeb0
zsonagy <gerzsonbigdata@gmail.com>
2019-02-10 18:12:53 +0100

    update package

--- run.sh
+++
@@ -1,2 +0,0 @@
-#java -jar target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:51000
-yarn jar target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar
